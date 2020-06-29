253d49ba80328b693acc48403916266a38c06304
Derek Lieu <dl@dereklieu.com>
2020-06-28 22:25:37 -0700

    Hello world app

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="scroll-hidden h-full relative">
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './app/index.js';
+
+require('./assets/css/assembly.min.css');
+
+ReactDOM.render(<App />, document.querySelector('body'));
