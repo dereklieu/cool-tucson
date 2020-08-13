35bcaba973199f1ae5377d62b56157cf2cc3fc21
Derek Lieu <dl@dereklieu.com>
2020-08-12 21:41:51 -0700

    Add placeholder cost in descriptions as well

--- src/interventions/description.js
+++ src/interventions/description.js
@@ -16,14 +16,20 @@ let Description = (props) => {
 
   const pillClassName = c(
     pillClass(intervention.type),
-    'flex-child txt-s ml12'
+    'flex-child txt-s mr12 mb24'
+  );
+
+  const currencyClassName = c(
+    pillClass('$'),
+    'flex-child txt-s mb24'
   );
 
   return (
     <div className={c(className, 'w600 hmax360 scroll-auto scroll-styled')}>
-      <div className="flex-parent flex-parent--end-cross mb24">
-        <h3 className="flex-child txt-h3 txt-bold">{intervention.name}</h3>
+      <div className="flex-parent flex-parent--center-cross flex-parent--wrap">
+        <h3 className="flex-child txt-h3 txt-bold mr12 mb24">{intervention.name}</h3>
         <div className={pillClassName}>{intervention.type}</div>
+        <div className={currencyClassName}>$$</div>
       </div>
       <div className="prose">
         <p>{intervention.description}</p>
