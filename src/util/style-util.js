b53991a7c74a9d8696f2bd470848c7e417ebe8e4
Derek Lieu <derek.lieu@mapbox.com>
2020-09-15 13:21:52 -0700

    Style utility returns configurable intervention colors

--- src/util/style-util.js
+++ src/util/style-util.js
@@ -5,15 +5,29 @@ export const px = (number) => Math.floor(number) + 'px';
 export const pct = (number) => number + '%';
 export const vw = (number) => number + 'vw';
 
+const interventionColor = {
+  'Private buildings': 'bg-blue',
+  'Town hall': 'bg-purple',
+  'Open area': 'bg-green',
+  'Street': 'bg-gray',
+  '$': 'bg-black'
+};
+
+const interventionHoverColor = {
+  'Private buildings': 'bg-blue-faint bg-blue-on-hover',
+  'Town hall': 'bg-purple-faint bg-purple-on-hover',
+  'Open area': 'bg-green-faint bg-green-on-hover',
+  'Street': 'bg-darken10 bg-gray-on-hover',
+  '$': 'bg-black'
+};
+
+export const getInterventionColor = (type, hover) => {
+  return hover ? interventionHoverColor[type] : interventionColor[type];
+}
+
 export const pillClass = (type) => {
   return c(
     'round px6 py3 color-white txt-bold',
-    {
-      'bg-blue': type === 'Private buildings',
-      'bg-purple': type === 'Town hall',
-      'bg-green': type === 'Open area',
-      'bg-gray': type === 'Street',
-      'bg-black': type === '$'
-    }
+    getInterventionColor(type, false)
   );
 }
