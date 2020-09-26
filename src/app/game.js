3974dd8a37e48260b47d741de55a6be405d458e7
Derek Lieu <derek.lieu@mapbox.com>
2020-09-26 15:49:12 -0700

    Remove bin

--- src/app/game.js
+++ src/app/game.js
@@ -8,21 +8,20 @@ import { Description } from '../interventions/description';
 import { Interventions } from '../interventions';
 import { Board } from '../board';
 import { Badges } from '../badges';
-import { Bin } from '../board/bin';
 import { Score } from '../indicators/score';
 import { Tooltip } from '../indicators/tooltip';
 
 export const Game = () => (
   <>
     <DndProvider backend={HTML5Backend}>
-      <Bin className="w-full viewport-full absolute top left flex-parent flex-parent--column flex-parent--end-main gradient-red">
+      <div className="w-full viewport-full absolute top left flex-parent flex-parent--column flex-parent--end-main gradient-red">
         <div className="flex-child scroll-hidden">
           <Board />
         </div>
         <div className="flex-child px3 py3 bg-black">
           <Score />
         </div>
-      </Bin>
+      </div>
       <div className="absolute top left w-full">
         <Menu />
         <Interventions />
